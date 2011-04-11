c035bf33a69569e60f3836389a7199dc9cd47ee7
Vinita <vinita.khandelwal@gmail.com>
2011-04-11 05:05:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shareit::Application.config.secret_token = 'd20fc1d59782b1c0d0e3e4e790c0184fe00bf5b418e4f87ac4914f41dbb1b80c8581c76d57ea1f4946b639feabb617c817e7e1259c06c929ddb9127b9d473c6f'
