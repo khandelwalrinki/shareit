25f4169a1704561673ac143f84cc374114b01c16
Vinita Rathi <vinita.khandelwal@gmail.com>
2011-04-27 09:26:00 +0100

    FInished layout and routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+    it "should have a home page at '/'" do
+      get '/'
+      response.should have_selector('title',:content => "Home")
+    end
+    
+    it "should have contact page at '/contact'" do
+      get '/contact'
+      response.should have_selector('title',:content => "Contact")
+    end
+
+    it "should have contact page at '/help'" do
+      get '/help'
+      response.should have_selector('title',:content => "Help")
+    end
+
+    it "should have signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector('title',:content => "Sign up")
+    end
+
+  end
+
