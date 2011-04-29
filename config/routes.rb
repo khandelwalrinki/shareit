5573206b5e0af1a39709a1f7e9cde072b3d9698e
Vinita Rathi <vinita.khandelwal@gmail.com>
2011-04-29 13:16:32 +0100

    added user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Shareit::Application.routes.draw do
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
-  
+  resources :microposts, :only => [:create, :destroy]
 
   match '/contact', :to=> 'pages#contact'
   match '/about', :to => 'pages#about'
